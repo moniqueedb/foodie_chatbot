269f04b98e3c06b4b036d50786372f11b9d552ab
Monique B <monibrown@teksystems.com>
2022-09-08 15:34:05 -0400

    deleting unnecessary text from flask file

--- flask_server.py
+++ flask_server.py
@@ -7,29 +7,6 @@ from chatbot.app import NeuralNet
 
 app = Flask(__name__)
 
-try:
-	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-	with open('chatbot/intents.json', 'r') as f:
-		intents = json.load(f)
-
-	FILE = "data.pth"
-	data = torch.load(FILE)
-
-	input_size = data["input_size"]
-	hidden_size = data["hidden_size"]
-	output_size = data["output_size"]
-	all_words = data["all_words"]
-	tags = data["tags"]
-	model_state = data["model_state"]
-
-	model = NeuralNet(input_size, hidden_size, output_size).to(device)
-	model.load_state_dict(model_state)
-	model.eval()
-
-except Exception as e:
-	print(e)
-
 
 @app.route('/')
 def home():
