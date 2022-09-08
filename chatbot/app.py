61d37df6b373d422b4927422190f634bb196accf
Monique B <monibrown@teksystems.com>
2022-09-08 15:46:15 -0400

    changed name of bot on line 31

--- chatbot/app.py
+++ chatbot/app.py
@@ -4,28 +4,31 @@ import torch
 from nltk_utils import bag_of_words, tokenize
 from model import NeuralNet
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+try:
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('chatbot/intents.json', 'r') as f:
-    intents = json.load(f)
+	with open('chatbot/intents.json', 'r') as f:
+		intents = json.load(f)
 
-FILE = "data.pth"
-data = torch.load(FILE)
+	FILE = "data.pth"
+	data = torch.load(FILE)
 
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data["all_words"]
-tags = data["tags"]
-model_state = data["model_state"]
+	input_size = data["input_size"]
+	hidden_size = data["hidden_size"]
+	output_size = data["output_size"]
+	all_words = data["all_words"]
+	tags = data["tags"]
+	model_state = data["model_state"]
 
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
+	model = NeuralNet(input_size, hidden_size, output_size).to(device)
+	model.load_state_dict(model_state)
+	model.eval()
 
+except Exception as e:
+	print(e)
 
 # chat
-bot_name = "R2D2"
+bot_name = "Foodie Bot"
 print("Let's chat! type 'quit' to exit")
 while True:
     sentence = input('You: ')
